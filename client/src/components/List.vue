103dcf57b91d3aaa0c0e7330e50f2f8fdf3fe244
Remizov Maksim <remizov.cs@gmail.com>
2020-09-24 11:33:05 +0300

    refactor: delete packages and libs

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -1,20 +1,28 @@
 /* eslint-disable */
 <template>
-  <div class="hello">    
+  <div class="hello">
     <div class="row">
       <div class="col-md-6 mx-auto">
         <table class="table">
-          <tr v-for="todo in todos" v-bind:key="todo._id" v-bind:task_name="todo.task_name">
+          <tr
+            v-for="todo in todos"
+            v-bind:key="todo._id"
+            v-bind:task_name="todo.task_name"
+          >
             <td class="text-left">{{ todo.task_name }}</td>
-            <td class="text-right">              
-              <button v-on:click="deleteTask(todo._id)" class="btn btn-danger">Delete</button>
+            <td class="text-right">
+              <button v-on:click="deleteTask(todo._id)" class="btn btn-danger">
+                Delete
+              </button>
             </td>
           </tr>
         </table>
         <h1 class="text-center">Генератор кода</h1>
-        <i>{{myCode}}</i>
-        <form v-on:submit.prevent="addNewTask" v-if="this.isEdit == false">          
-          <button type="submit" class="btn btn-success btn-block mt-3">Сгенерировать код</button>
+        <i>{{ myCode }}</i>
+        <form v-on:submit.prevent="addNewTask" v-if="this.isEdit == false">
+          <button type="submit" class="btn btn-success btn-block mt-3">
+            Сгенерировать код
+          </button>
         </form>
         <form v-on:submit.prevent="updateTask" v-else>
           <label for="tasknameinput">Enter your code</label>
@@ -24,90 +32,91 @@
             class="form-control"
             placeholder="Enter your code"
           />
-          <button type="submit" class="btn btn-success btn-block mt-3">Отправить</button>
+          <button type="submit" class="btn btn-success btn-block mt-3">
+            Отправить
+          </button>
         </form>
       </div>
     </div>
   </div>
 </template>
 
-
 <script>
-import axios from "axios";
+import axios from 'axios'
 
 export default {
   data() {
     return {
       todos: [],
-      id: "",
-      taskname: "",
+      id: '',
+      taskname: '',
       isEdit: false,
-      myCode: "",
-    };
+      myCode: '',
+    }
   },
 
   mounted() {
-    this.getTasks();
+    this.getTasks()
   },
 
   methods: {
     getTasks() {
-      axios({ method: "GET", url: "/api/tasks" }).then(
+      axios({ method: 'GET', url: '/api/tasks' }).then(
         (result) => {
-          console.log(result);
-          this.todos = result.data.obj1;
+          console.log(result)
+          this.todos = result.data.obj1
         },
         (error) => {
-          console.error(error);
+          console.error(error)
         }
-      );
-    },   
+      )
+    },
 
     addNewTask() {
       axios
-        .post("/api/task", {})
+        .post('/api/task', {})
         .then((res) => {
-          this.getTasks();
-          this.isEdit = true;
-          this.myCode = "";
+          this.getTasks()
+          this.isEdit = true
+          this.myCode = ''
         })
         .catch((err) => {
-          console.log(err);
-        });
-    },    
+          console.log(err)
+        })
+    },
 
-    updateTask() {      
+    updateTask() {
       axios
         .put(`/api/task/`, { task_name: this.taskname })
-        .then((res) => {          
-          const finResult = mess => {
-            this.taskname = "";
-            this.isEdit = false;
-            this.myCode = mess;
-          }          
+        .then((res) => {
+          const finResult = (mess) => {
+            this.taskname = ''
+            this.isEdit = false
+            this.myCode = mess
+          }
           if (res.data.length === 1) {
-            finResult("Great!");
+            finResult('Great!')
           } else {
-            finResult('Your code is not validate');
+            finResult('Your code is not validate')
           }
         })
         .catch((err) => {
-          console.log(err);
-        });
+          console.log(err)
+        })
     },
 
     deleteTask(id) {
       axios
         .delete(`/api/task/${id}`)
         .then((res) => {
-          this.taskname = "";
-          this.getTasks();
-          console.log(res);
+          this.taskname = ''
+          this.getTasks()
+          console.log(res)
         })
         .catch((err) => {
-          console.log(err);
-        });
+          console.log(err)
+        })
     },
   },
-};
+}
 </script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -9,7 +9,7 @@ export default new Router({
     {
       path: '/',
       name: 'List',
-      component: List
-    }
-  ]
+      component: List,
+    },
+  ],
 })
